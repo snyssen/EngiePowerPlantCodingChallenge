04a97eb33901606e1fc2a50727d8e4d820f25b3e
Simon Nyssen <dev@snyssen.be>
2023-05-15 08:24:59 +0200

    Remove SmartEnum library and replace usage by regular enums
    I wanted to use FromValue method but it did not work as expected, so I implemented my own FromString

--- src/EngiePowerPlantCodingChallenge.WebApi/Enums/FuelType.cs
+++ src/EngiePowerPlantCodingChallenge.WebApi/Enums/FuelType.cs
@@ -6,15 +6,33 @@ using Ardalis.SmartEnum;
 
 namespace EngiePowerPlantCodingChallenge.WebApi.Enums
 {
-    public class FuelType : SmartEnum<FuelType, string>
+    public enum FuelType
     {
-        public static FuelType Gas => new(nameof(Gas), "gas");
-        public static FuelType Kerosine => new(nameof(Kerosine), "kerosine");
-        public static FuelType Co2 => new(nameof(Co2), "co2");
-        public static FuelType Wind => new(nameof(Wind), "wind");
+        Gas,
+        Kerosine,
+        Co2,
+        Wind
 
-        public FuelType(string name, string value) : base(name, value)
-        {
-        }
+        // public static FuelType Gas => new(nameof(Gas), "gas");
+        // public static FuelType Kerosine => new(nameof(Kerosine), "kerosine");
+        // public static FuelType Co2 => new(nameof(Co2), "co2");
+        // public static FuelType Wind => new(nameof(Wind), "wind");
+
+        // public FuelType(string name, string value) : base(name, value)
+        // {
+        // }
+    }
+
+    public static class FuelTypeHelper
+    {
+        public static FuelType FromString(string type)
+            => type switch
+            {
+                "gas" => FuelType.Gas,
+                "kerosine" => FuelType.Kerosine,
+                "co2" => FuelType.Co2,
+                "wind" => FuelType.Wind,
+                _ => throw new NotSupportedException($"FuelType {type} is not supported")
+            };
     }
 }
--- src/EngiePowerPlantCodingChallenge.WebApi/Enums/PowerPlantType.cs
+++ src/EngiePowerPlantCodingChallenge.WebApi/Enums/PowerPlantType.cs
@@ -2,16 +2,31 @@ using Ardalis.SmartEnum;
 
 namespace EngiePowerPlantCodingChallenge.WebApi.Enums
 {
-    public class PowerPlantType : SmartEnum<PowerPlantType, string>
+    public enum PowerPlantType
     {
-        public static PowerPlantType GasFired => new(nameof(GasFired), "gasfired");
-        public static PowerPlantType TurboJet => new(nameof(TurboJet), "turbojet");
-        public static PowerPlantType WindTurbine => new(nameof(WindTurbine), "windturbine");
+        GasFired,
+        TurboJet,
+        WindTurbine
+        // public static PowerPlantType GasFired => new(nameof(GasFired), "gasfired");
+        // public static PowerPlantType TurboJet => new(nameof(TurboJet), "turbojet");
+        // public static PowerPlantType WindTurbine => new(nameof(WindTurbine), "windturbine");
 
-        public PowerPlantType(string name, string value) : base(name, value)
-        {
-        }
+        // public PowerPlantType(string name, string value) : base(name, value)
+        // {
+        // }
 
-        // TODO: Add FuelType ?
+        // // TODO: Add FuelType ?
+    }
+
+    public static class PowerPlantTypeHelper
+    {
+        public static PowerPlantType FromString(string type)
+            => type switch
+            {
+                "gasfired" => PowerPlantType.GasFired,
+                "turbojet" => PowerPlantType.TurboJet,
+                "windturbine" => PowerPlantType.WindTurbine,
+                _ => throw new NotSupportedException($"PowerPlantType {type} is not supported")
+            };
     }
 }
